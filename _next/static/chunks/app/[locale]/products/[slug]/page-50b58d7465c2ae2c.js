e3d32248a2c9b50f7737cf40b9c051ba13b31309
cvdgauss-beep <sales@ai3613.com>
2026-09-12 08:48:52 +0800

    Deploy ai3613.com static site (en/ar/de) - 2026-09-11 build

---
+++ _next/static/chunks/app/[locale]/products/[slug]/page-50b58d7465c2ae2c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[309],{347:function(e,n,s){Promise.resolve().then(s.bind(s,5247)),Promise.resolve().then(s.bind(s,3333)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[231,173,458,971,23,744],function(){return e(e.s=347)}),_N_E=e.O()}]);
