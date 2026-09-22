42c5af46d2e21ffa90810467a1437b646944ee16
cvdgauss-beep <cvdgauss@gmail.com>
2026-09-22 21:43:13 +0800

    Use static-safe navigation across all three language versions

---
+++ _next/static/chunks/app/[locale]/page-deb8ad52a512186e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61],{4297:function(e,n,s){Promise.resolve().then(s.bind(s,5247)),Promise.resolve().then(s.bind(s,3333)),Promise.resolve().then(s.t.bind(s,8173,23))}},function(e){e.O(0,[173,458,971,23,744],function(){return e(e.s=4297)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/[locale]/products/[slug]/page-7255b5484ae853ce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[309],{4328:function(e,n,s){Promise.resolve().then(s.bind(s,5247)),Promise.resolve().then(s.bind(s,3333))}},function(e){e.O(0,[173,458,971,23,744],function(){return e(e.s=4328)}),_N_E=e.O()}]);
